3c1ac2efd93324597bd3882daf9fed61e4657f2b
ATRI0721 <3543322350@qq.com>
2025-12-14 14:27:32 +0800

    更改后端CORS配置

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Psycal Story API", version="1.0.0")
 # CORS配置
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost" , "http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
